c9fcb2b09dae8b9d98130e4ecccad2a61a35ddf8
SanskarDuttSharma <sanskar.dutt78@gmail.com>
2021-09-25 22:38:01 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "../App.css";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Package from "../components/Package";
+import Services from "../components/Services";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Map from "../components/Map/Map";
+class Home extends Component {
+  render() {
+    return (
+      <div id='home'>
+        <Hero />
+        <About />
+        <Map />
+        {/* <Package /> */}
+        <Services />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Home;
